8870319c24560eb08297a9d403a43d3ccbf5e378
surekha <surekhasrao@gmail.com>
2018-03-17 20:43:08 +0000

    Added query strings offset and count to stocks.controller.js

--- api/controllers/stocks.controllers.js
+++ api/controllers/stocks.controllers.js
@@ -2,7 +2,22 @@ var stockData = require('../data/stock-data.json');
 
 module.exports.stocksGetAll = function(req, res){
     console.log("GET the stocks");
-        res
-            .status(200)
-            .json(stockData);
+    console.log(req.query);
+    
+    var offset = 0;
+    var count = 5;
+    
+    if(req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+    
+    if(req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+    
+    var returnData = stockData.slice(offset, offset + count);
+
+    res
+        .status(200)
+        .json(returnData);
 };
