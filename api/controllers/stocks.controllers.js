7ce16c01dd2ef9ca882aab213a472dc7990d8cac
surekha <surekhasrao@gmail.com>
2018-03-18 20:58:34 +0000

    meannasdaq database connected to app.js

--- api/controllers/stocks.controllers.js
+++ api/controllers/stocks.controllers.js
@@ -1,9 +1,10 @@
+var dbconn = require('../data/dbconnection.js');
 var stockData = require('../data/stock-data.json');
 
 module.exports.stocksGetAll = function(req, res){
-    console.log("GET the stocks");
-    console.log(req.query);
-    
+    var db = dbconn.get();
+    var collection = db.collection('stocks');
+
     var offset = 0;
     var count = 5;
     
@@ -13,11 +14,26 @@ module.exports.stocksGetAll = function(req, res){
     
     if(req.query && req.query.count) {
         count = parseInt(req.query.count, 10);
-    }
+    }    
+    collection
+        .find()
+        .toArray(function(err, docs){
+            console.log('Found stocks', docs);
+            res
+                .status(200)
+                .json(docs);
+        });
+};    
+//     console.log('db', db);
     
-    var returnData = stockData.slice(offset, offset + count);
+//     console.log("GET the stocks");
+//     console.log(req.query);
+    
 
-    res
-        .status(200)
-        .json(returnData);
-};
+    
+//     var returnData = stockData.slice(offset, offset + count);
+
+//     res
+//         .status(200)
+//         .json(returnData);
+// };
