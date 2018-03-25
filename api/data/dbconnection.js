d60a94f14fdb6c4cff678c3683318bce31f4081f
surekha <surekhasrao@gmail.com>
2018-03-25 20:31:32 +0000

    Implementation of backend User authentication

--- api/data/dbconnection.js
+++ api/data/dbconnection.js
@@ -35,31 +35,4 @@ process.once('SIGUSR2', function(){
 
 //BRING IN SCHEMAS AND MODELS
 require('./stocks.model.js');
-
-
-//Native-driver connection
-// var MongoClient = require('mongodb').MongoClient;
-// var dburl = 'mongodb://surekhasrinivasan-firstworkspace-5563392';
-
-// var _connection = null;
-
-// var open = function(){
-//     MongoClient.connect(dburl, function(err, client){
-//         if(err) {
-//             console.log("DB connection failed");
-//             return;
-//         }
-//     // set _connection
-//         _connection = client.db("meannasdaq");
-//         console.log("DB connection open", client);
-//     });
-// };
-
-// var get = function() {
-//     return _connection;
-// };
-
-// module.exports = {
-//     open : open,
-//     get : get
-// };
+require('./users.model.js');
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 var ctrlStocks = require('../controllers/stocks.controllers.js');
+var ctrlUsers = require('../controllers/users.controllers.js');
+
 router
     .route('/stocks')
     .get(ctrlStocks.stocksGetAll);
@@ -9,5 +11,14 @@ router
 router
     .route('/stocks/:stockId')
     .get(ctrlStocks.stocksGetOne);
+
+//Authentication routes
+router
+    .route('/users/register')
+    .post(ctrlUsers.register);
+
+router
+    .route('/users/login')
+    .post(ctrlUsers.login);
     
 module.exports = router;
