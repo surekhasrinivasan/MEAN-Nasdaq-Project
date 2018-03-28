20de19c1864ad3b3644e8b813dea38d5514b3cb4
surekha <surekhasrao@gmail.com>
2018-03-28 20:55:12 +0000

    Worked on main.html, main-page-controller.js

---
+++ public/angular-app/main/main-page-controller.js
@@ -0,0 +1,7 @@
+/*global angular*/
+angular.module('meannasdaq').controller('MainPageController', MainPageController);
+
+function MainPageController() {
+    var vm = this;
+    vm.title = 'Welcome to MEAN Nasdaq Webpage!';
+}
